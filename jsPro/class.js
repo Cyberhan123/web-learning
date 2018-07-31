561fb61fcbee1c67bcc3912d6d18bc8a8907c706
Cyberhan123 <255542417@qq.COM>
2018-07-31 14:58:55 +0800

    jq实验函数中~~~

---
+++ jsPro/class.js
@@ -0,0 +1,20 @@
+function Father() {
+    this.name = 'xxx';
+}
+
+//原型对象
+Father.prototype.eat = function () {
+    console.log(this.name);
+};
+var p = new Father();
+p.eat();
+
+function Son(age) {
+    this.age = age;
+    console.log(this.age);
+}
+//继承
+Son.prototype = new Father();
+
+var s = new Son(1234);
+s.eat();

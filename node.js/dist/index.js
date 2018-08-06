cd4290f268a7245ccd421fd20e865fe47c558561
Cyberhan123 <255542417@qq.COM>
2018-08-06 11:46:58 +0800

    ajax/yield node.js/babel(es6->es5)

---
+++ node.js/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var a = void 0;
+var xxx = 5;
+a = function a(set) {
+    console.log(set);
+    console.log(xxx);
+};
---
+++ node.js/src/index.js
@@ -0,0 +1,6 @@
+let a;
+const xxx=5;
+a=set=>{
+    console.log(set);
+    console.log(xxx);
+};

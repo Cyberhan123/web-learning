72b74d07e2a206d8cb3b0ee369ca9aa7994cbb95
Cyberhan123 <255542417@qq.COM>
2018-08-14 11:27:37 +0800

    vue-cli

---
+++ vue/vuecli3/src/views/My.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script>
+    import HelloWorld from '@/components/HelloWorld.vue';
+    export default {
+        name: "My",
+        components: {
+            HelloWorld,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
